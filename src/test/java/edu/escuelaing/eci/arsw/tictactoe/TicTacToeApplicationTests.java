ea8bacb8ae69e212689f4d4748902bcf6dee2a2a
Csarssj <cesar.gonzalez-c@mail.escuelaing.edu.co>
2020-06-30 22:26:45 -0500

    Tutorial terminado

---
+++ src/test/java/edu/escuelaing/eci/arsw/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.eci.arsw.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
